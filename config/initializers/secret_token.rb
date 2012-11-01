bff93c3de2d911b9c236987981774b1d89bfba56
Hiroshi Susa <susa12@gmail.com>
2012-11-01 14:25:52 +0900

    config commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Carfare::Application.config.secret_token = 'c3cdd0d4673049bc2f7fcb3197d54ea85768936cdfa8a7346f440a63f1c4ad9b360beddaca636aa25bd902271bbf419298cd32951d78f41bfb9cf05e6b91fb09'
