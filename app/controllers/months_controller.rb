cc73dd19b654003ed205634d3d9200106e4ab7d4
Hiroshi Susa <susa12@gmail.com>
2012-11-07 12:45:58 +0900

    Month corresponded to Ajax.

--- app/controllers/months_controller.rb
+++ app/controllers/months_controller.rb
@@ -3,9 +3,36 @@ class MonthsController < ApplicationController
   def create
     @owner = Owner.find(params[:owner_id])
     @month = Owner.find(params[:owner_id]).months.new
+    @month.year = params[:month]['year']
     @month.month = params[:month]['month']
     @month.save
     redirect_to owner_path(@owner)
   end
+  
+  def show
+    @owner = Owner.find(params[:owner_id])
+    @month = Month.find(params[:id])
+  end
+  
+  def edit
+    @month = Month.find(params[:id])
+  end
+  
+  def update
+    @month = Month.find(params[:id])
+    @month.assign_attributes(params[:month])
+    if @month.save
+      redirect_to action: "show"
+    else
+      redirect_to action: "edit"
+    end
+  end
+  
+  def destroy
+    @month = Month.find(params[:id])
+    @month.destroy
+    redirect_to action: "show"
+  end
+  
 
 end
