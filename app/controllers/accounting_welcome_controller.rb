ccb19250daaf253b5e893a58a731f1c4a301f3ae
Hiroshi Susa <susa12@gmail.com>
2013-01-22 16:41:29 +0900

    controller add # -*- encoding: utf-8 -*-

--- app/controllers/accounting_welcome_controller.rb
+++ app/controllers/accounting_welcome_controller.rb
@@ -1,3 +1,4 @@
+# -*- encoding: utf-8 -*-
 class AccountingWelcomeController < ApplicationController
   def index
   	if current_accounting  
--- app/controllers/admin_welcome_controller.rb
+++ app/controllers/admin_welcome_controller.rb
@@ -1,3 +1,4 @@
+# -*- encoding: utf-8 -*-
 class AdminWelcomeController < ApplicationController
   def index
     if current_admin  
